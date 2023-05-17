a965d2d90a69b1bafd12a4e0e66ea22d37527b72
LAPTOP-6UTEKP1F\qwert <hks1321@naver.com>
2023-05-17 17:17:42 +0900

    [신희경] - 최초 파일 추가

---
+++ src/main/java/com/heekyung/memoboard/MemoboardApplication.java
@@ -0,0 +1,13 @@
+package com.heekyung.memoboard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MemoboardApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MemoboardApplication.class, args);
+	}
+
+}
